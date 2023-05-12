17fb7e0246bf9f587acbe8055388ab1049fa6598
Favour <mzfavvy20@gmail.com>
2023-05-12 07:10:18 -0700

    fixing 4th code

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,10 +2,8 @@
 if __name__ == '__main__':
     import dis as dis_module
     import hidden_4
-    
+
     names = [name for name in dir(hidden_4) if not name.startswith("__")]
     sorted_names = sorted(names)
-
-
     for name in sorted_names:
         print(name)
