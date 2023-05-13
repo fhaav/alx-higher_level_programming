a900a5885e8a4aad83b4bedff67f285bf252cfee
Favour <mzfavvy20@gmail.com>
2023-05-13 12:08:59 -0700

    updated

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
-    # Check if the ASCII value of the character is within the lowercase range (97-122)
     if 97 <= ord(c) <= 122:
         return True
     return False
