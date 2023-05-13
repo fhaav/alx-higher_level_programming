3d4f4ea629e2cd294f700f0e2c563c467f439cde
Favour <mzfavvy20@gmail.com>
2023-05-13 09:58:36 -0700

    update

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for letter in range(ord('a'), ord('z') + 1):
-    print(chr(letter), end='')
+alpha = ""
+for i in range(26):
+    alpha = alpha + chr(ord("a") + i)
+print(f"{alpha}", end="")
