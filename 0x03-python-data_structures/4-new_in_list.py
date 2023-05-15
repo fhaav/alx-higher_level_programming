933e65388e4ac9588ffd9df696fe900fda9aa8f7
Favour <mzfavvy20@gmail.com>
2023-05-15 08:52:07 -0700

    Implemented solution for replacing an element in a list without modifying the original list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    num_elements = len(my_list)
+
+    if num_elements == 0 or idx < 0 or num_elements - 1 < idx:
+            return my_list
+
+        my_list[idx] = element
+        return my_list
