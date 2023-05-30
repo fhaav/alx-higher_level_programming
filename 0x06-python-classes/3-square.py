cb2a1c76c3424d1408bfeef75d4005a40815b27a
Favour <mzfavvy20@gmail.com>
2023-05-30 11:09:16 -0700

    implemented a python script that defines a square based on 2-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""This script defines a class called Square."""
+
+
+class Square:
+    """Represents a square object."""
+
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        def area(self):
+            """
+            Calculates the area of the square.
+
+            Returns:
+                The current square area.
+            """
+            return (self.__size ** 2)
