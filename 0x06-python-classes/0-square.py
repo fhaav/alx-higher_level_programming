d4a9e039e5a8c38ee9a6b2d53f63ad17194cff14
Favour <mzfavvy20@gmail.com>
2023-05-30 08:42:59 -0700

    implemented a Python script that defines an empty class.

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""This script defines a class called Square."""
+
+
+class Square:
+    """Represents a square object."""
+
+    def __init__(self):
+        """Initializes a new instance of the Square class."""
+        pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+This directory contains Python scripts and code examples pertaining to the subject of classes in Python, a fundamental aspect of object-oriented programming (OOP).
