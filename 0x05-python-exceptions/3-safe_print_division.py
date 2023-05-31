8214651b31ba7d6a50fdc6ec2ec4a861e0f8b39f
Favour <mzfavvy20@gmail.com>
2023-05-31 08:26:39 -0700

    Implemented a function for integers division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    """
+    Divides 2 integers and prints the result.
+
+    Args:
+        a (int): The numerator.
+        b (int): The denominator.
+
+    Returns:
+        The value of the division, None otherwise.
+    """
+    try:
+        result = a / b
+    except (ZeroDivisionError, TypeError):
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return (result)
