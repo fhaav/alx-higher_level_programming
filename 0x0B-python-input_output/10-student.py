9156d3fa6a1524a37df0a8405c7647986aecaafd
Favour <mzfavvy20@gmail.com>
2023-06-13 18:44:01 -0700

    Added Student class with to_json() method supporting attribute filtering

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+Module containing Student class
+"""
+
+
+class Student:
+    """ Class representing a student. """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student object.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Returns a dictionary representation of the student instance.
+        """
+        if isinstance(attrs, list):
+            return {key: getattr(self, key) for key in attrs}
+        return self.__dict__
