0d26cf34dd60ef225e44e52bc89fee5150ff2eca
Favour <mzfavvy20@gmail.com>
2023-06-13 19:17:14 -0700

    Implement reload_from_json method in Student class

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+"""
+Module containing Student class
+"""
+
+
+class Student:
+    """ Class representing a student. """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student object.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+        def to_json(self, attrs=None):
+            """
+            Returns a dictionary representation of the student instance.
+            """
+            if isinstance(attrs, list):
+                return {key: self.__dict__[key]
+                        for key in self.__dict__ if key in attrs}
+                return self.__dict__
+
+        def reload_from_json(self, json):
+            for key, value in json.items():
+                self.__dict__[key] = json[key]
