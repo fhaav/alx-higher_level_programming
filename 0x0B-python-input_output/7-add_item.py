e02eb91d7da1d0a1e6701685bcd4c6da3451fadd
Favour <mzfavvy20@gmail.com>
2023-06-13 17:48:52 -0700

    made some corrections in 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,23 +9,19 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-def add_items_to_list():
-    """
-    Adds command-line arguments to a Python list and saves them to a file.
-    """
+filename = "add_item.json"
+new_items = sys.argv[1:]
+existing_items = []
 
-    filename = "add_item.json"
-    new_items = sys.argv[1:]
+
+try:
+    with open(filename, 'r') as file:
+        content = file.read()
+        if len(content) > 0:
+            existing_items = load_from_json_file(filename)
+except FileNotFoundError:
     existing_items = []
 
-    try:
-        with open(filename, 'r') as file:
-            content = file.read()
-            if len(content) > 0:
-                existing_items = load_from_json_file(filename)
-    except FileNotFoundError:
-        existing_items = []
+updated_items = existing_items + new_items
 
-    updated_items = existing_items + new_items
-
-    save_to_json_file(updated_items, filename)
+save_to_json_file(updated_items, filename)
