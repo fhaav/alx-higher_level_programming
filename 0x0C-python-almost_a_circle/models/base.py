b0399a0abaf9ee364efe4c5fc346ae5093a2a87a
Favour <mzfavvy20@gmail.com>
2023-06-22 08:07:54 -0700

    Updated the class Base by adding the class method def save_to_file(cls, list_objs)

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,6 +5,8 @@ Contains the Base class.
 """
 
 import json
+import csv
+import os
 
 
 class Base:
@@ -33,3 +35,23 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        filename = cls.__name__ + ".json"
+        if list_objs is None:
+            list_objs = []
+        json_list = [obj.to_dictionary() for obj in list_objs]
+        json_string = cls.to_json_string(json_list)
+        with open(filename, "w") as file:
+            file.write(json_string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        Returns a list of instances from a JSON string representation
+        """
+        if json_string is None or json_string == "":
+            return []
+
+        return json.loads(json_string)
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,8 +6,11 @@ Contains Unittests for the Base class
 
 
 import unittest
+import json
+import os
 from models.base import Base
 from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase(unittest.TestCase):
@@ -16,7 +19,7 @@ class TestBase(unittest.TestCase):
     def test_single_instance(self):
         """ Test creation of a single instance without custom ID """
         b1 = Base()
-        self.assertEqual(b1.id, 6)
+        self.assertEqual(b1.id, 1)
 
     def test_multiple_instances(self):
         """ Test creation of multiple instances without custom ID """
@@ -48,9 +51,50 @@ class TestBase(unittest.TestCase):
         r1 = Rectangle(10, 7, 2, 8)
         dictionary = r1.to_dictionary()
         json_string = Base.to_json_string([dictionary])
-        expect_json_string = '[{"id": 7, "width": 10, "height": 7, ' \
+        expect_json_string = '[{"id": 2, "width": 10, "height": 7, ' \
             '"x": 2, "y": 8}]'
         self.assertEqual(json_string, expect_json_string)
+
+    def test_from_json_string(self):
+        """ Test the from_json_string method """
+        json_string = Base.from_json_string(None)
+        self.assertEqual(json_string, [])
+
+        json_string = Base.from_json_string("")
+        self.assertEqual(json_string, [])
+
+        json_string = (
+            '[{"id": 1, "width": 10, "height": 7, "x": 2, "y": 8}, '
+            '{"id": 2, "width": 2, "height": 4, "x": 0, "y": 0}]'
+        )
+        expected_list = [
+            {"id": 1, "width": 10, "height": 7, "x": 2, "y": 8},
+            {"id": 2, "width": 2, "height": 4, "x": 0, "y": 0}
+        ]
+        obj_list = Base.from_json_string(json_string)
+        self.assertEqual(obj_list, expected_list)
+
+    def test_save_to_file(self):
+        """ Test the save_to_file method """
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 4)
+        Rectangle.save_to_file([r1, r2])
+
+        with open("Rectangle.json", "r") as file:
+            json_string = file.read()
+            obj_list = json.loads(json_string)
+            expected_list = [
+                {"id": 6, "width": 10, "height": 7, "x": 2, "y": 8},
+                {"id": 7, "width": 2, "height": 4, "x": 0, "y": 0}
+            ]
+            self.assertEqual(obj_list, expected_list)
+
+        # Clean the created file
+        Base._Base__nb_objects = 0
+        try:
+            os.remove("Rectangle.json")
+        except FileNotFoundError:
+            pass
 
 
 if __name__ == '__main__':
