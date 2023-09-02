5d15d5091ec5cb712351207c58f148ac8a7f46f1
fhaav <mzfavvy20@gmail.com>
2023-09-01 17:14:01 -0700

    Task 2 completed

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Sends a POST request with an email parameter to a given URL and
+# displays the response body (decoded in utf-8).
+
+import urllib.parse
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    url, email = sys.argv[1], sys.argv[2]
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+
+    with urllib.request.urlopen(url, data=data) as response:
+        print(response.read().decode('utf-8'))
