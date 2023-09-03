326d5ed47767a0825eea49e9da1ce152879f6c15
fhaav <mzfavvy20@gmail.com>
2023-09-03 14:49:25 -0700

    3-my_safe_filter_states.py completed

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+This script retrieves and lists all states from the hbtn_0e_0_usa database.
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_prefix = sys.argv[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+                         passwd=mysql_password, db=database_name)
+    cursor = db.cursor()
+    cursor.execute(
+            """SELECT * FROM states
+            WHERE states.name = %s ORDER BY states.id
+            """, (sys.argv[4], )
+    )
+
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
