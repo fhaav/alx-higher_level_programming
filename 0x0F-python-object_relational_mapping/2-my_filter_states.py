00101cc41662611d0ea85b72ff7d9891ced148af
fhaav <mzfavvy20@gmail.com>
2023-09-03 14:21:24 -0700

    made corrections

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 
 """
-This script retrieves and lists all states from the hbtn_0e_0_usa database
-and prints them in ascending order by their IDs.
+This script retrieves and lists all states from the hbtn_0e_0_usa database.
 """
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_prefix = sys.argv[4]
+
     db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
                          passwd=mysql_password, db=database_name)
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
                    .format(sys.argv[4]))
-
+ii
     states = cursor.fetchall()
     for state in states:
         print(state)
