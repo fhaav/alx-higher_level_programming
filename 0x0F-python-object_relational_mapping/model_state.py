72390bd9a40abd23fe7fdf096f1ec7bc130c4c8e
fhaav <mzfavvy20@gmail.com>
2023-09-03 15:57:59 -0700

    correction

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,7 +9,6 @@ Base = declarative_base()
 
 class State(Base):
     """ Represents a state entity in the database. """
-     __tablename__ = 'states'
-
-     id = Column(Integer, unique=True, nullable=False, primary_key=True)
-     name = Column(String(128), nullable=False)
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
