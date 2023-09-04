03b6faf5881bdd4ea1390df510d6c91bb4ad240b
fhaav <mzfavvy20@gmail.com>
2023-09-03 18:22:47 -0700

    101-relationship_states_cities_list completed

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -12,10 +12,13 @@ from model_city import City
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     newState = State(name='California')
     newState.cities = [City(name='San Francisco')]
+
     session.add(newState)
+
     session.commit()
